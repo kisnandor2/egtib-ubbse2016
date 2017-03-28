3666ae83ca42cc9a02fe3e0f6266a1ef2926724d
kisnandor2 <kisnandor3@gmail.com>
2017-03-28 16:51:53 +0300

    Line ending again

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#check if on master
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
 	docker login -u="$DOCKER_USERNAME" -p="$api_key" registry.heroku.com
 	docker build -t registry.heroku.com/egtib/web .
--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+#start the test
 npm test
