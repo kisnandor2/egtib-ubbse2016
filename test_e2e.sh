87259fc8e1f1539348b2d2881b4ffd7061146799
kisnandor2 <kisnandor3@gmail.com>
2017-03-29 21:43:45 +0300

    Server killed after tests, change to chrome

--- test_e2e.sh
+++ test_e2e.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 if [ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
+	npm start & ServerPID=$! #start the server and save its PID
 	npm run test_e2e
+	kill $ServerPID #kill the server after test
 	exit 0
 fi
 
 if [ "$TRAVIS_BRANCH" = "dev" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
+	npm start & ServerPID=$! #start the server and save its PID
 	npm run test_e2e
+	kill $ServerPID #kill the server after test
 	exit 0
 fi
 
