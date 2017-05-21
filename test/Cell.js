ab9399d2474dfa4d8c9022fbda169616d86601d2
BorosEdmund <borosedmund@gmail.com>
2017-05-21 12:46:52 +0300

    3 more unit test

--- test/Cell.js
+++ test/Cell.js
@@ -44,7 +44,7 @@ describe('Cell', function() {
             }
 	    });
     //-------------------------------------------------------------------------------------
-        it('should return true when the cell1 is divided', function(){
+        it('should return true when the cell1 is divided nr.1', function(){
 		    let cell1 = new Cell({
 				x: 4,
 				y: 4,
@@ -62,6 +62,75 @@ describe('Cell', function() {
             let divideCell=cell1.divideCell([cell2],1, myRandomGenerator);
             
             if(divideCell[0].x == 3.5 && divideCell[1].x==4.5 && divideCell[0].y==4 && divideCell[1].y==4)
+                assert.isTrue(true);
+            else
+                assert.isTrue(false);
+
+	    });
+        it('should return true when the cell1 is divided nr.2', function(){
+		    let cell1 = new Cell({
+				x: 2,
+				y: 6,
+				attrib: 'c',
+				cost: 0.1,
+				bbox: [2,2]
+			});
+            let cell2 =new Cell({
+				x: 1,
+				y: 8,
+				attrib: 'd',
+				cost: 0.4,
+				bbox: [4,5]
+			});
+            let divideCell=cell1.divideCell([cell2],1, myRandomGenerator);
+          
+            if(divideCell[0].x == 1.75 && divideCell[1].x==2.25 && divideCell[0].y==6 && divideCell[1].y==6)
+                assert.isTrue(true);
+            else
+                assert.isTrue(false);
+
+	    });
+ it('should return true when the cell1 is divided check cost nr.1', function(){
+		    let cell1 = new Cell({
+				x: 2,
+				y: 6,
+				attrib: 'c',
+				cost: 0.1,
+				bbox: [2,2]
+			});
+            let cell2 =new Cell({
+				x: 1,
+				y: 8,
+				attrib: 'd',
+				cost: 0.4,
+				bbox: [4,5]
+			});
+            let divideCell=cell1.divideCell([cell2],1, myRandomGenerator);
+          
+            if(divideCell[0].cost == 0.1 && divideCell[1].cost == 0.1)
+                assert.isTrue(true);
+            else
+                assert.isTrue(false);
+
+	    });
+ it('should return true when the cell1 is divided check cost nr.2', function(){
+		    let cell1 = new Cell({
+				x: 2,
+				y: 6,
+				attrib: 'c',
+				cost: 0.6,
+				bbox: [2,2]
+			});
+            let cell2 =new Cell({
+				x: 1,
+				y: 8,
+				attrib: 'd',
+				cost: 0.8,
+				bbox: [4,5]
+			});
+            let divideCell=cell1.divideCell([cell2],1, myRandomGenerator);
+          
+            if(divideCell[0].cost == 0.6 && divideCell[1].cost == 0.6)
                 assert.isTrue(true);
             else
                 assert.isTrue(false);
